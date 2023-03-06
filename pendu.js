64c9daaf48fea3ee6629d6088e447e5b8602078e
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-03-06 22:38:05 +0100

    floor et commentaires

--- pendu.js
+++ pendu.js
@@ -7,4 +7,8 @@ const messageFinal = document.getElementById('message-final');
 
 const figurePartie = document.querySelectorAll('.figure-partie');
 
-const mots = ['urluberlu','kaleidoscope','libellule','fifrelin','implémentation','concaténer']
+const mots = ['urluberlu','kaleidoscope','libellule','fifrelin','implémentation','concaténer'];
+
+//selection un mot pour jouer
+let motSelectionne = mots[Math.floor(Math.random() * mots.length)] //length parce qu'on ne veut pas un mot random 14 (14e mot dans l'array) si on à seulement 6 mots).
+//floor arrondit au plus bas.
