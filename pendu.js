d1a5db0d12d8a83c1fdd854cdc97056521a0d76f
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-03-06 22:15:22 +0100

    creation des constantes utiles en JS

---
+++ pendu.js
@@ -0,0 +1,6 @@
+const motEl = document.getElementById('mot');
+const mauvaisesLettres = document.getElementById('mauvaises-lettres');
+const rejouerBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-contenant');
+const notification = document.getElementById('notification-contenant');
+const messageFinal = document.getElementById('message-final');
