3bad8e7dbc265a4b404df81cabe736e21e9d0e2e
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-03-06 23:32:35 +0100

    separer les lettre pour pouvoir les passer une à la fois

--- pendu.js
+++ pendu.js
@@ -9,6 +9,33 @@ const figurePartie = document.querySelectorAll('.figure-partie');
 
 const mots = ['urluberlu','kaleidoscope','libellule','fifrelin','implémentation','concaténer'];
 
-//selection un mot pour jouer
-let motSelectionne = mots[Math.floor(Math.random() * mots.length)] //length parce qu'on ne veut pas un mot random 14 (14e mot dans l'array) si on à seulement 6 mots).
+// SELECTIONNER UN MOT POUR JOUER 
+
+let motSelectionne = mots[Math.floor(Math.random() * mots.length)]; //length parce qu'on ne veut pas un mot random 14 (14e mot dans l'array) si on à seulement 6 mots).
 //floor arrondit au plus bas.
+
+const bonnesLettresArr = [''];
+const mauvaisesLettresArr = [];
+
+//Afficher le mot caché
+
+function afficherMot() {
+    motEl.innerText = ` 
+        ${motSelectionne
+            .split('')  //séparé par lettre
+            .map(   //passer chaque lettre une à la fois
+                lettre =>`
+                    <span class="lettre">
+                        ${bonnesLettresArr.includes(lettre) ? lettre :''}   // ? est comme un if. Si la partie avant contient le lettre alors on affiche la lettre (= lettre après l'?) sinon on n'affiche rien.
+                        //
+                    </span>
+                `
+            )
+        }
+    `;
+}
+
+afficherMot();
+
+
+
